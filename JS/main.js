6215515e7a98a0fc9b9b251546cc9d1f4775d267
denisgat <denisgatotho@gmail.com>
2020-03-03 16:55:46 -0500

    color changes

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+let button2 = document.getElementById('btn2')
+
+
+
+function weather(){
+    var zip = document.getElementById('zip').value
+    console.log(zip);
+
+}
+
+button2.onclick = weather;
