b5deb5cc1a128841a7a1086827d558f96cbc135a
denisgat <denisgatotho@gmail.com>
2020-03-03 19:46:25 -0500

    authentication

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,17 @@
 let button2 = document.getElementById('btn2')
+var numbers = /^[0-9]+$/;
 
 
 
 function weather(){
     var zip = document.getElementById('zip').value
-    console.log(zip);
+        if (zip.length == 5 && zip.match(numbers)){
+            console.log(zip);
+            w3.hide('body');
+        }
+        else{
+            console.log('The zip code you entered was invalid')
+        }
 
 }
 
