2f200bc07839ec3bb08f0498f88fbdd8380e1d75
denisgat <denisgatotho@gmail.com>
2020-03-03 22:35:21 -0500

    error message

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,21 @@
 let button2 = document.getElementById('btn2')
 var numbers = /^[0-9]+$/;
 var key = '03c33881e2e5600f85fd019355ff5915';
+var alert = document.getElementById('alert')
 
 
 
 
 function weather(){
     var zip = document.getElementById('zip').value
-        if (zip.length == 5 && zip.match(numbers)){
-            console.log(zip);
+        if (zip.length == 0){
+            alert.innerHTML = 'Please enter your zipcode'
+        }
+        else if (zip.length == 5 && zip.match(numbers)){
+            alert.innerHTML = zip
         }
         else{
-            console.log('The zip code you entered is invalid')
+            alert.innerHTML = ('The zip code you entered is invalid')
         }
 
 }
